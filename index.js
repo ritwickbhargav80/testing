c641cf1e4515242c75d9e057bbd0ce73869dbe49
RitwickBhargav <47817908+RitwickBhargav@users.noreply.github.com>
2019-06-24 17:10:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const index = require('./app');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-
+1
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 const connectionString = "mongodb://ritwick80:ritwick123@cluster0-shard-00-00-rloq9.mongodb.net:27017,cluster0-shard-00-01-rloq9.mongodb.net:27017,cluster0-shard-00-02-rloq9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
